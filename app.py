00d8c257230413cac2b11d23b696e96384a0b42c
today <135026855+quthery@users.noreply.github.com>
2024-02-08 14:41:07 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from jutsu import JutSu, get_link_and_download
+
+
+async def main():
+    slug = input("Отправь ссылку или имя(для вставки ctrl+shift+v): ")
+
+    if slug.startswith("http"):
+        slug = slug.split("/")[3]
+
+    res = input("Выбери разрешение(1080, 720, 480, 360): ")
+
+    download_type = "1"
+
+    if res not in ("1080", "720", "480", "360"):
+        return print("Выбери правильноек разрешение")
+
+    jutsu = JutSu(slug)
+
+    episodes = await jutsu.get_all_episodes()
+
+    if download_type == "1":
+        for episode in episodes:
+            await get_link_and_download(jutsu, episode, res)
+
+    # elif download_type == "2":
+    #     coros = [
+    #         asyncio.create_task(get_link_and_download(jutsu, episode, res))
+    #         for episode in episodes
+    #     ]
+    #     await asyncio.gather(*coros)
+
+    else:
+        print(":/")
+
+    await jutsu.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ jutsu.py
@@ -0,0 +1,66 @@
+import os
+from typing import List
+
+import aiofiles
+import aiohttp
+from bs4 import BeautifulSoup
+
+from config import DIR, HEADERS, LINK
+
+DIR = input("выбери папку в которую будет загружаться аниме: ")
+class Episode:
+    def __init__(self, episode_name: str, href: str) -> None:
+        self.name = episode_name
+        self.href = href
+
+        self.season = href.split("/")[2] if "season" in href else "season-1"
+
+
+class JutSu:
+    def __init__(self, slug: str) -> None:
+        self.slug = slug
+        self.client = aiohttp.ClientSession(headers=HEADERS)
+
+    async def close(self) -> None:
+        await self.client.close()
+
+    async def get_all_episodes(self) -> List[Episode]:
+        main_page = await self.client.get(f"{LINK}/{self.slug}")
+
+        soup = BeautifulSoup(await main_page.text(), "html.parser")
+
+        episodes = soup.find_all("a", {"class": "short-btn"})
+
+        return [Episode(episode.text, episode.attrs["href"]) for episode in episodes]
+
+    async def get_download_link(self, href: str, res: str) -> str:
+        episode_page = await self.client.get(f"{LINK}/{href}")
+
+        soup = BeautifulSoup(await episode_page.text(), "html.parser")
+
+        source = soup.find("source", {"res": res})
+        source = source if source else soup.find("source")
+
+        return source.attrs["src"] if source else None
+
+
+async def get_link_and_download(inst: JutSu, episode: Episode, res: str):
+    link = await inst.get_download_link(episode.href, res)
+
+    if link:
+        await download_video(link, f"{DIR}/{inst.slug}/сезон-{episode.season[7]}/{episode.name}.mp4")
+
+
+async def download_video(link: str, path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs("/".join(path.split("/")[:-1]), exist_ok=True)
+
+    print(f"Начинаю загрузку {path}")
+
+    async with aiohttp.ClientSession(raise_for_status=True, headers=HEADERS) as cli:
+        async with cli.get(link) as r:
+            async with aiofiles.open(path, "wb+") as f:
+                async for d in r.content.iter_any():
+                    await f.write(d) if d else None
+
+    print(f"{path} Готово!")
